ff979f3f0cf9b2c235ac30a7d5a294949158e32e
jotave42 <jvictorcostamelo@gmail.com>
2019-06-10 05:57:36 -0300

    added now responsding

--- Complete.py
+++ Complete.py
@@ -1,4 +1,16 @@
+import json
 file = open("lavajato.pl","r")
-text = file.read()
+itens =[]
+lines = file.readlines()
+for line in lines:
+        newline =line.replace("\n","")
+        if(not newline in itens):
+                itens.append(newline)
 file.close()
-print(text)
+print(len(itens))
+jsonFile = open("graph.json","r")
+text = jsonFile.read()
+jsonFile.close()
+jsonData = json.loads(text)
+for i in itens:
+    print(i)
--- graph.py
+++ graph.py
@@ -7,46 +7,55 @@ class Graph:
     def __init__(self, indice, name):
         self.nome = name
         self.indice = indice
-
+        self.aresta =[]
+        self.peso =[]
+def LulaTaPreso(graphs):
+    for sujeito in graphs:
+        if(sujeito.nome == "luiz_inacio_lula_da_silva"):
+            for i in range(len(sujeito.aresta)):
+                if(sujeito.peso[i]=="condenadopor" or sujeito.peso[i]=="condenadopelo"):
+                    return "SIM"
+            return "NAO"
+def MoroCondenaCabral(graphs):
+    for sujeito in graphs:
+        if(sujeito.nome == "sergio_moro"):
+            for i in range(len(sujeito.aresta)):
+                if(sujeito.peso[i]=="condena" and sujeito.aresta[i].nome =="sergio_cabral"):
+                    return "SIM"
+            return "NAO"
+def ListaRelacoes(nome,graphs):
+    for sujeito in graphs:
+        if(sujeito.nome == nome):
+            for i in range(len(sujeito.aresta)):
+                print(nome," -> ",sujeito.peso[i]," por/com --> ",sujeito.aresta[i].nome)
 jsonFile = open("graph.json","r")
 text = jsonFile.read()
+jsonFile.close()
 jsonData =json.loads(text)
-print(jsonData)
+#print(jsonData)
 graphs=[]
 for iten in jsonData:
     grap= Graph(iten["indice"],iten["nome"])
     graphs.append(grap)
-    print(iten["indice"])
-    print(iten["nome"])
-    print(iten)
-print("======================")
+    #print(iten["indice"])
+    #print(iten["nome"])
+    #print(iten)
+#print("======================")
 for iten in jsonData:
     aresta = iten["aresta"]
     if(len(aresta)>0):
         ind = iten["indice"]
         pesos = iten["peso"]
-        print("aresta")
-        print("ind=>",ind)
-        print(aresta)
-        print(pesos)
-        print(len(aresta))
-        print(len(pesos))
-        
         for i in range(len(aresta)):
-            graphs[i].aresta.append(graphs[aresta[i]])
-            graphs[i].peso.append(pesos[i])
+            graphs[ind].aresta.append(graphs[aresta[i]])
+            graphs[ind].peso.append(pesos[i])
+    pesos =[]
+    aresta =[]
+for sujeito in graphs:
+    if(sujeito.nome == "sergio_moro"):
+            for i in range(len(sujeito.aresta)):
+                print("moro -> ",sujeito.peso[i]," por/com --> ",sujeito.aresta[i].nome)
 
-        
-    
-for gr in graphs:
-    print(gr)
-    print(gr.nome)
-    print(gr.indice)
-    if(len(gr.aresta)>0):
-        print("tem aresta")
-        print(gr.aresta)
-        for i in range(len(gr.aresta)):
-            print()
-            print("nome => ",gr.nome)
-            print("eh => ",gr.peso[i])
-            print("por / com =>",gr.aresta[i].nome)
+print("LULA TA PRESO ? ",LulaTaPreso(graphs))
+print("MORO CONDENA CABRAL ? ",MoroCondenaCabral(graphs))
+ListaRelacoes("aaa")
